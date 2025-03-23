638737cac179c00e9a0ae980c464e5e2114c3d0c
ChienDainn <chien.minh001.com>
2025-03-23 22:53:31 +0700

    Add project files.

---
+++ AuthTrain/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace AuthTrain
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<IdentityUser>(options)
+    {
+       
+    }
+  
+}
---
+++ AuthTrain/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthTrain.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        [HttpGet("")]
+        [Authorize]
+        public async Task<IActionResult> GetData()
+        {
+            return Ok("U made it");
+        }
+    }
+}
